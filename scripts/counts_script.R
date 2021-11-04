c86b1df9be243928f0077f8ce6d213d36271cc16
lenmetson <lennardmetson@gmail.com>
2021-11-04 12:01:39 +0000

    added a couple of lines to R script just to start to illustrate what it might look like

--- scripts/counts_script.R
+++ scripts/counts_script.R
@@ -5,3 +5,7 @@
 
 if(!require("tidyverse")) {install.packages("tidyverse"); library("tidyverse")}
 if(!require("sylcount")) {install.packages("sylcount"); library("sylcount")}
+
+speeches <- # file path and relevant function
+
+syls <- sylcount(speeches$speeches)
