9ed087afc98a59f7066b87a1501f9ad54694c554
lenmetson <lennardmetson@gmail.com>
2021-11-19 16:13:58 +0000

    LM: added script to download file progromatically

---
+++ scripts/download-csv-html.R
@@ -0,0 +1,7 @@
+# specify the url
+url <- "https://dataverse.harvard.edu/api/access/datafile/4432889"
+
+# download the file into a specified path - add your analysis folder in between " "
+download.file(url, "")
+
+
