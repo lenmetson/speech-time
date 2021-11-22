0ef05c6d7d3b034e2dc25a5e1e21a9a247501682
lenmetson <lennardmetson@gmail.com>
2021-11-22 13:07:10 +0000

    added .gitignore for data_raw

--- scripts/download-csv-html.R
+++ scripts/download-csv-html.R
@@ -2,6 +2,6 @@
 url <- "https://dataverse.harvard.edu/api/access/datafile/4432889"
 
 # download the file into a specified path - add your analysis folder in between " "
-download.file(url, "")
+download.file(url, here("data_raw", "rawdata.csv"))
 
 
