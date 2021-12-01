65a57d404361d517565793090797bd671e8ecbe3
Noémie Claret <36684852+nclaret1@users.noreply.github.com>
2021-12-01 21:47:05 +0000

    Update prelim-analysis-jupyter.py

--- scripts/prelim-analysis-jupyter.py
+++ scripts/prelim-analysis-jupyter.py
@@ -99,3 +99,91 @@ print(total_words_men)
 
 print(total_words_women/total_words_men)
 
+# Create a dictionary with the propotion of women words to men words indexed by year
+
+first create a dictionary with the years (seems useless but just to check we are acutally getting years where things happened)
+
+
+dates=find_unique_values(speech_table, "date", "none", "none", False)
+print(dates)
+
+years=[]
+for date in dates:
+    year=date[0:4]
+    if year not in years:
+        years.append(year)
+    else:
+        continue
+print(years)
+    
+
+define a function to find values in a column with two filters
+
+def find_values_bis(dataset, column, feature1, feature2, feature3, feature4,comment=True):
+    if feature1!="none":
+        dataset=dataset[dataset[feature1]==feature2][dataset[feature3]==feature4]
+    values=dataset[column]
+    if comment==True:
+        print("The {} with the following filter: {} is {} are given below.".format(column, feature1, feature2))
+    return values
+
+Now get the proportion of words spoken by women to men each year
+
+get new column with year
+
+speech_table["year"]=speech_table["date"].str[0:4]
+print(speech_table["year"])
+
+year_words_women=[]
+for year in years:
+    total_words_women_series=find_values_bis(speech_table, "speech_word_count", "gender", "F","year", year, False)
+    total_words_women=sum(total_words_women_series)
+    year_words_women.append(total_words_women)
+
+freq_table_words_women_byyear=pd.DataFrame({"years":years, "words":year_words_women})
+print(freq_table_words_women_byyear)
+
+
+year_words_men=[]
+for year in years:
+    total_words_men_series=find_values_bis(speech_table, "speech_word_count", "gender", "M","year", year, False)
+    total_words_men=sum(total_words_men_series)
+    year_words_men.append(total_words_men)
+
+freq_table_words_men_byyear=pd.DataFrame({"years":years, "words":year_words_men})
+print(freq_table_words_women_byyear)
+
+import numpy as np
+data={"years":years, "words_ratio":np.array(year_words_women)/np.array(year_words_men)}
+ratio_words_gender_byyear=pd.DataFrame(data)
+print(ratio_words_gender_byyear)
+
+# But these results may not mean much
+We need to use the proportion of women to men each year and see if the increase in words spoken is just due to the increase in the number of women
+
+
+number_women_year=[]
+for year in years:
+    number_women_series=find_unique_values_bis(speech_table, "name", "gender", "F","year", year, False)
+    number_women=len(number_women_series)
+    number_women_year.append(number_women)
+print(number_women_year)
+
+number_men_year=[]
+for year in years:
+    number_men_series=find_unique_values_bis(speech_table, "name", "gender", "M","year", year, False)
+    number_men=len(number_men_series)
+    number_men_year.append(number_men)
+print(number_men_year)
+
+ratio_words_gender_byyear["ratio_women_men"]=np.array(number_women_year)/np.array(number_men_year)
+ratio_words_gender_byyear["words_spoken_to_gender_distribution"]=ratio_words_gender_byyear["words_ratio"]/ratio_words_gender_byyear["ratio_women_men"]
+
+print(ratio_words_gender_byyear)
+
+# Plot for visualization
+
+import matplotlib.pyplot as plt
+plt.plot(ratio_words_gender_byyear["years"],ratio_words_gender_byyear["words_spoken_to_gender_distribution"])
+
+
