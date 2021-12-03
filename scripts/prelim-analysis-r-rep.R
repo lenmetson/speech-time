8f30da4ae19519fc25ea03f1385a0eed287f2948
Len Metson <lennardmetson@gmail.com>
2021-12-03 19:50:33 +0000

    filtered script

--- scripts/prelim-analysis-r-rep.R
+++ scripts/prelim-analysis-r-rep.R
@@ -6,4 +6,10 @@ source(here("scripts", "r-packages"))
 
 # Load data
 
-harvard <- read.csv(here("data_raw", ""))
+harvard <- read.csv(here("data_raw", "rawdata.csv"))
+
+speeches <- harvard %>% filter(is_speech == 1)
+speeches <- speeches %>% filter(gender == "M" | "F")
+# reconfigure to drop all without parl_id ?
+
+speeches
