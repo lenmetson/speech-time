473deecb05ec2a96ffc52a889fb44591ac63f4e4
Len Metson <lennardmetson@gmail.com>
2021-12-07 22:48:09 +0000

    update r

--- scripts/prelim-analysis-r-rep.R
+++ scripts/prelim-analysis-r-rep.R
@@ -11,8 +11,6 @@ speeches <- harvard %>% filter(is_speech == 1)
 rm(harvard)
 speeches <- speeches %>% filter(!is.na(parl_id)) # drop all without parl_id
 
-# explore different types of speeches
-#types <- data.frame(unique(speeches$item))
 
 # define counts of speeches
 n_speeches <- nrow(speeches)
@@ -37,4 +35,14 @@ MSPs_all <- speeches %>%
 source(here("scripts", "analysis-by-parly.R"))
 
 
+# n_m | n_f | p_m | p_f
+n_m <- c(n_speeches_m1, n_speeches_m2, n_speeches_m3, n_speeches_m4, n_speeches_m5)
+n_f <- c(n_speeches_f1, n_speeches_f2, n_speeches_f3, n_speeches_f4, n_speeches_f5)
+p_m <- c(prop_speeches_m1, prop_speeches_m2, prop_speeches_m3, prop_speeches_m4, prop_speeches_m5) 
+p_f <- c(prop_speeches_f1, prop_speeches_f2, prop_speeches_f3, prop_speeches_f4, prop_speeches_f5)
 
+summary <- data.frame(n_m, n_f, p_m, p_f)
+
+
+# explore different types of speeches
+types <- data.frame(unique(speeches$item))
