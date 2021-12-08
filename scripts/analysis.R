4df8f2b28574bde2d3586c177f81adda1d831438
Len Metson <lennardmetson@gmail.com>
2021-12-08 13:47:10 +0000

    add new script

---
+++ scripts/analysis.R
@@ -0,0 +1,9 @@
+#
+if(!require("here"))install.packages("here")
+# We need to source our summary data
+#source(here("scripts", "download-csv-http.R"))# If you haven't downloaded the data yet, uncomment this line
+# Next source the script that creates the summary data
+#source(here("scripts", "prelim-analysis-r-rep.R")) # takes quite a long time to run
+# If you have already run, comment out and load summary data directly
+
+data <- read.csv(here("output_data", "summary.csv"))
