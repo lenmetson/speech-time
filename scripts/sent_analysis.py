fe94d74bc8818bc4ccf6da678ec9a2c3d2076352
Noémie Claret <36684852+nclaret1@users.noreply.github.com>
2022-01-08 14:50:21 +0000

    first iteration, cleaning data

---
+++ scripts/sent_analysis.py
@@ -0,0 +1,91 @@
+#following workshop from
+#https://www.youtube.com/watch?v=U8m5ug9Q54M
+
+
+
+import pandas as pd
+
+df=pd.read_csv("/Users/noemieclaret/Desktop/feliz_navidad.csv")
+
+#convert each speech into lower case
+
+lower_case_speeches=[]
+def to_lower_case_speech(speech):
+    for speech in csv['speech']:
+        for word in speech:
+            speech_lower=[]
+            speech_lower.append(str.lower(word))
+        lower_case_speeches.append(speech)
+        
+
+#tokenize
+from nltk import sent_tokenize, word_tokenize
+import nltk
+nltk.download('punkt')
+
+
+def tokenize_sent_speech(speech):
+    sent_tok=[]
+    for sent in speech:
+        sent=sent.tokenize(sent)
+        sent_tok.append(sent)
+        return sent_tok
+        
+def tokenize_words_from_sentence(sent_tok):
+    words_tok=[]
+    for sentences in sent_tok:
+        tokens = word_tokenize(sentences)
+        words_tok.append(tokens)
+        
+#remove weird characters
+import re
+
+def remove_weird_characters(speech):
+    sent_tok=tokenize_sent_speech(speech)
+    clean_speech1=[]
+    for sentence in sent_tok:
+        clean_sentence=[]
+        words_tok=tokenize_words_from_sentence(sentence)
+        for tokens in words_tok:
+            clean=[]
+            for w in tokens:
+                res=re.sub(r'[^\w\s]', "", w)
+                if res!="":
+                    clean.append(res)
+            clean_sentence.append(clean)
+        clean_speech.append(clean_sentence)
+        
+    
+        
+       
+        
+#remove stopwords
+
+
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+         
+def remove_stopwords(clean_speech1):
+    clean_speech2=[]
+    for sentence in speech:
+        t=[]
+        for token in sentence:
+            if not token in stopwords.words('english'):
+                t.append(token)
+        clean_speech2.append(t)
+        
+        
+        
+        
+#stemming
+
+from nltk.stem.porter import PorterStemmer   
+port=PorterStemmer()   
+
+def stem_speech(clean_speech2):
+    clean_speech3=[]
+    for sentence in clean_speech2:
+        stemmed_tokens=[port.stem(i) for i in sentence]
+        clean_speech3.append(stemmed_tokens)
+        
+        
