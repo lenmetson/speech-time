e623cd3d53cc76bd7a304d5d7bd1d8cb9965f192
Lennard Metson <74358730+lenmetson@users.noreply.github.com>
2022-01-13 17:04:04 +0000

    added content to moodle

--- scripts/r-analysis/content-analysis.R
+++ scripts/r-analysis/content-analysis.R
@@ -2,7 +2,7 @@ speeches_syl$speech <- speeches$speech %>% tolower() %>% removePunctuation()
 
 speeches_syl$about_gen <- NA
 
-gen_dict <- "woman*|women*|female*|femin*|sexism|equal* pay|period poverty|sex* assault|menstrat*|abortion|matern*|contraception|reproductive rights|pro-life|pro-choice"
+gen_dict <- "feminis*|feminin*|patriarc*|sexis*|misogyn*|misandr*|victim blam*|women|woman|gender action plan|gender balance|gender based violence|gender bias*|gender equity|gender equality|gender gap|pay gap|gender rol*|gender stereotyp*|menstrual hygiene|sexual right*|reproductive right*|abortion*|sex work*|gender discrimina*|female*|girl*|sexual violence|sexual assaults|rape*|domestic violence|woman*|women*|female*|femin*|sexism|equal* pay|period poverty|sex* assault|menstrat*|abortion|matern*|contraception|reproductive rights|pro-life|pro-choice"
 
 speeches_syl$about_gen <-  as.integer(str_detect(speeches$speech, gen_dict))
 
